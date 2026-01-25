f3c800d3ccc2216b3448623cec6e3f46a71a62df
Araz Abishov <araz@abishov.com>
2026-01-25 18:52:20 +0100

    chore: deleing first implementation

--- src/index.js
+++ src/index.js
@@ -2,13 +2,6 @@
 
 import { Command } from "commander";
 import { consola } from "consola";
-import { workspace } from "./workspace.js";
-import { extensions } from "./fs.js";
-import { extname } from "path";
-import { deduplicate } from "./deduplicate.js";
-import CliTable3 from "cli-table3";
-import { transform } from "./transform.js";
-import { scan } from "./stages/scan.js";
 import { runPipeline } from "./pipeline.js";
 import { logger } from "./utils/logger.js";
 
@@ -31,6 +24,8 @@ program
   )
   .option("--strict", false)
   .option("--skip-dedupe", "skip deduplication step")
+  .option("--skip-embed", "skip metadata embedding step")
+  .option("--skip-rename", "skip rename step")
   .option("--no-fuzzy", "disable fuzzy matching for metadata linking");
 
 program.parse(process.argv);
@@ -62,65 +57,6 @@ if (options.source && options.destination) {
   }
 
   await runPipeline(options);
-
-  // // Phase 1: scan files, calculate hashes, and link metadata
-  // const project = await workspace(options.source);
-
-  // // Collect stats for transparency
-  // const projectStats = {
-  //   images: 0,
-  //   imagesWithoutMetadata: 0,
-  //   videos: 0,
-  //   videosWithoutMetadata: 0,
-  //   others: 0,
-  // };
-
-  // for (const mediaFile of project.media.values()) {
-  //   const { entry } = mediaFile.media;
-  //   const ext = extname(entry.name).toLowerCase();
-
-  //   if (extensions.images.includes(ext)) {
-  //     projectStats.images += 1;
-
-  //     if (!mediaFile.metadata) {
-  //       projectStats.imagesWithoutMetadata += 1;
-  //     }
-  //   } else if (extensions.videos.includes(ext)) {
-  //     projectStats.videos += 1;
-
-  //     if (!mediaFile.metadata) {
-  //       projectStats.videosWithoutMetadata += 1;
-  //     }
-  //   } else {
-  //     projectStats.others += 1;
-  //   }
-  // }
-
-  // const projectStatsTable = new CliTable3({ head: ["Category", "Count"] });
-  // projectStatsTable.push(
-  //   ["Total media files", project.media.size],
-  //   ["Images (total/no metadata)", `${projectStats.images}/${projectStats.imagesWithoutMetadata}`],
-  //   ["Videos (total/no metadata)", `${projectStats.videos}/${projectStats.videosWithoutMetadata}`],
-  //   ["Albums", project.albums.size],
-  //   ["Other files", project.unsupportedEntries.size + projectStats.others]
-  // );
-
-  // // Using console instead of consola here to ensure proper formatting.
-  // console.info(projectStatsTable.toString());
-
-  // // For a new line
-  // console.info();
-
-  // const mediaLibrary = deduplicate(project);
-
-  // // For a new line
-  // console.info();
-
-  // // Copy and embed metadata at destination
-  // await transform(mediaLibrary, options);
-
-  // // For a new line
-  // console.info();
 
   consola.success("Finished!");
 } else {
